28b5434bf67c4224887d2e322890a1274a435829
chan <chan@ilooyo.com>
2015-10-11 14:31:45 +0800

    finish caesar encrypt

--- caesar.cpp
+++ caesar.cpp
@@ -1,13 +1,35 @@
-#include<stdio.h>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
 
 int main(int argc, char const *argv[])
 {
 	int caesarNum = 0;
-	char *source;
+	string sourceMessage;
 
-	for (int i = 0; i < argc; ++i)
+	if (argc == 1)
 	{
-		printf("%s \n", argv[i]);
+		caesarNum = 0;
 	}
+	else
+	{
+		caesarNum = atoi(argv[1]);
+	}
+	caesarNum %= 26;
+
+	cout << "input the source message: \n";
+	cin >> sourceMessage;
+	string encryptedMessage = sourceMessage;
+	for (auto &a : encryptedMessage)
+	{
+		a = (a + caesarNum) > 'z' ? (a + caesarNum - 26) : (a + caesarNum);
+	}
+
+	cout << "The caesar number is : " << caesarNum <<
+		"\nThe source meassage is :"<< sourceMessage <<
+		"\nThe encrypted message is :" << encryptedMessage << endl;
+	
 	return 0;
 }
